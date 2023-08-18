10bbb3c89a42abdf19b21c5876061211738b9a95
rafikulislam775 <islamrafikul.jp@gmail.com>
2023-08-18 20:38:50 +0900

    adding some function and expiriment & finish the  3 output areas

--- script/area-calculator.js
+++ script/area-calculator.js
@@ -8,18 +8,60 @@ function calculateTriangleArea() {
   const area = (baseValue * heightValue) / 2;
   //   console.log(area);
   const triangleArea = document.getElementById("triangle-area");
-  triangleArea.innerHTML = area;
+  triangleArea.innerText = area;
 }
 
 function calculateRectangleArea() {
   const widthField = document.getElementById("rectangle-width").value;
-  const widthValue = parseFloat(widthField);
+  // const widthValue = parseFloat(widthField);
   //   console.log(widthValue);
   const lengthField = document.getElementById("rectangle-length").value;
-  const lengthValue = parseFloat(lengthField);
+  // const lengthValue = parseFloat(lengthField);
   //   console.log(heightValue);
-  const area = widthValue * lengthValue;
-  //   console.log(area);
+  // const area = widthValue * lengthValue;
+  const areas = widthField * lengthField;
+  // console.log(areas);
+  // console.log(typeof areas);
   const rectangleArea = document.getElementById("rectangle-area");
-  rectangleArea.innerHTML = area;
+  rectangleArea.innerText = areas;
+}
+
+// reusable function to  get input value
+// function getInput(id) {
+//   const inputId = document.getElementByI(id);
+//   const input = inputId.value;
+//   console.log(input);
+//   return input;
+//   //
+// }
+
+// function calculateParallelogramArea() {
+//   const base = getInput("parallelogram-base");
+//   const height = getInput("parallelogram-height");
+//   const area = base.value * height.value;
+//   const parallelogramArea = document.getElementById("parallelogram-area");
+//   parallelogramArea.innerText = area;
+// }
+// Reusable function to get input value
+function getInput(id) {
+  const input = document.getElementById(id);
+  return input;
+}
+
+function calculateParallelogramArea() {
+  const base = getInput("parallelogram-base");
+  const height = getInput("parallelogram-height");
+  const area = base.value * height.value;
+  // console.log(area);
+  // console.log(typeof area);
+  // const parallelogramArea = document.getElementById("parallelogram-area");
+
+  // parallelogramArea.innerText = area;
+  // niser function diyr try korb
+  setElementInnerText("parallelogram-area", area);
+}
+// Reusable function to get and set p , div,etc for .innerText
+function setElementInnerText(id, area) {
+  const element = document.getElementById(id);
+  element.innerText = area;
 }

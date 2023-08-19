d81875d7f16a6b3630103c3d95f0508d9cadac4f
rafikulislam775 <islamrafikul.jp@gmail.com>
2023-08-20 06:48:14 +0900

    temporally finished

--- script/area-calculator.js
+++ script/area-calculator.js
@@ -9,21 +9,27 @@ function calculateTriangleArea() {
   //   console.log(area);
   const triangleArea = document.getElementById("triangle-area");
   triangleArea.innerText = area;
+  //adding calculation entry
+  calculateArea("triangle", area);
 }
 
 function calculateRectangleArea() {
   const widthField = document.getElementById("rectangle-width").value;
-  // const widthValue = parseFloat(widthField);
-  //   console.log(widthValue);
   const lengthField = document.getElementById("rectangle-length").value;
+  const area = widthField * lengthField;
+  // niser 2 ta ine deya na deya eki
+  // const widthValue = parseFloat(widthField);
   // const lengthValue = parseFloat(lengthField);
-  //   console.log(heightValue);
   // const area = widthValue * lengthValue;
-  const areas = widthField * lengthField;
-  // console.log(areas);
   // console.log(typeof areas);
-  const rectangleArea = document.getElementById("rectangle-area");
-  rectangleArea.innerText = areas;
+  if (isNaN(widthField) || isNaN(lengthField)) {
+    alert("Please enter a number");
+  } else {
+    const rectangleArea = document.getElementById("rectangle-area");
+    rectangleArea.innerText = area;
+  }
+  //adding calculation entry
+  calculateArea("rectangle", area);
 }
 
 // reusable function to  get input value
@@ -44,21 +50,31 @@ function calculateRectangleArea() {
 // }
 // Reusable function to get input value
 function getInput(id) {
-  const input = document.getElementById(id);
+  const input = document.getElementById(id).value;
   return input;
+  // const area = base * height;
+  // const input = document.getElementById(id);
+  //  const area = base .value * height.value;
 }
 
 function calculateParallelogramArea() {
   const base = getInput("parallelogram-base");
   const height = getInput("parallelogram-height");
-  const area = base.value * height.value;
+
   // console.log(area);
   // console.log(typeof area);
   // const parallelogramArea = document.getElementById("parallelogram-area");
-
+  if (isNaN(base) || isNaN(height)) {
+    alert("Please enter a number");
+    return;
+  }
+  const area = base * height;
   // parallelogramArea.innerText = area;
   // niser function diyr try korb
   setElementInnerText("parallelogram-area", area);
+
+  //adding calculation entry
+  calculateArea("parallelogram", area);
 }
 // Reusable function to get and set p , div,etc for .innerText
 function setElementInnerText(id, area) {
@@ -69,6 +85,28 @@ function setElementInnerText(id, area) {
 function calculateEllipseArea() {
   const major = getInput("ellipse-major-redus");
   const minor = getInput("ellipse-minor-redus");
-  const area = 3.1416 * major.value * minor.value;
-  setElementInnerText("ellipse-area", area);
+  const area = 3.1416 * major * minor;
+  const areaToDecimal = area.toFixed(2);
+  setElementInnerText("ellipse-area", areaToDecimal);
+  //adding calculation entry
+  calculateArea("ellipse", area);
+}
+
+// add to calculation entry
+// get the element you want to add to
+//if needs to add some class to the element
+//set the element's inner html. this dynamic template strings
+function calculateArea(AreaType, area) {
+  // console.log(AreaType + " " + area);
+  const calculateEntry = document.getElementById("calculateEntry");
+  const count = calculateEntry.childElementCount;
+  // add count
+  const p = document.createElement("p");
+  // add class
+  p.classList.add("my-4");
+  // p.innerHTML = AreaType + " " + area;
+  p.innerHTML = `${
+    count + 1
+  }.${AreaType}: ${area} cm² <button class="btn btn-sm btn-error">Convert</button>`;
+  calculateEntry.appendChild(p);
 }
